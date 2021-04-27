9e6ddb656c78ef9643c43ca9315b0188f7326d25
evan999 <emeshberge@gmail.com>
2021-04-27 10:40:12 -0400

    Updated solution. Solution complete. Passes all test cases.

--- ClimbingTheLeaderboard.java
+++ ClimbingTheLeaderboard.java
@@ -15,56 +15,37 @@ class Result {
     /*
      * Complete the 'climbingLeaderboard' function below.
      *
-     * The function is expected to return an INTEGER_ARRAY.
-     * The function accepts following parameters:
-     *  1. INTEGER_ARRAY ranked
-     *  2. INTEGER_ARRAY player
+     * The function is expected to return an INTEGER_ARRAY. The function accepts
+     * following parameters: 1. INTEGER_ARRAY ranked 2. INTEGER_ARRAY player
      */
-     
-     
 
     public static List<Integer> climbingLeaderboard(List<Integer> ranked, List<Integer> player) {
-    // Write your code here
-        List<Integer> rank = new ArrayList<>();
-        List<Integer> playerRank = new ArrayList<>();
-        
-        
-        for (int score = 0; score < ranked.size()-1; score++) {
-            if (ranked.get(score) == ranked.get(score+1)) {
-                ranked.remove(score+1);
-            }
-            rank.add(score+1);
-            //System.out.println(rank.get(score));
-            //System.out.println(ranked.get(score));
-        }
-        
-        
-        int ranking = rank.size()+1;
-        for (int score = 0; score < player.size(); score++) {
-            if (player.get(score) < ranked.get(score)) {
-                ranking++;
-                playerRank.add(ranking);
-                ranking = rank.size() + 1;
-                //continue;
-            }
-            else if (player.get(score) > ranked.get(score)) {
-                ranking--;
-            }
-            else if (player.get(score) == ranked.get(score)) {
-                playerRank.add(ranking);
-            }
-            
-        }
-        
-        /*
-        for (int score = 0; score < rank.size(); score++) {
-            System.out.println(rank.get(score));
-        }
-        */
-        return playerRank;
-        
-    }
+        // Write your code here
+        Set<Integer> rankedSet = new LinkedHashSet<>();
 
+        for (Integer score : ranked) {
+            rankedSet.add(score);
+        }
+
+        List<Integer> rankedScores = new ArrayList<>(rankedSet);
+        List<Integer> playerRanks = new ArrayList<>();
+
+        int rank = rankedScores.size() + 1;
+        for (Integer score : player) {
+            for (int ranking = rank - 2; ranking >= 0; ranking--) {
+                if (score < rankedScores.get(ranking)) {
+                    break;
+                } else if (score == rankedScores.get(ranking)) {
+                    rank--;
+                    break;
+                } else {
+                    rank--;
+                }
+            }
+            playerRanks.add(rank);
+        }
+        return playerRanks;
+    }
 }
 
 public class Solution {
@@ -75,23 +56,16 @@ public class Solution {
         int rankedCount = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> ranked = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+                .map(Integer::parseInt).collect(toList());
 
         int playerCount = Integer.parseInt(bufferedReader.readLine().trim());
 
         List<Integer> player = Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
-            .map(Integer::parseInt)
-            .collect(toList());
+                .map(Integer::parseInt).collect(toList());
 
         List<Integer> result = Result.climbingLeaderboard(ranked, player);
 
-        bufferedWriter.write(
-            result.stream()
-                .map(Object::toString)
-                .collect(joining("\n"))
-            + "\n"
-        );
+        bufferedWriter.write(result.stream().map(Object::toString).collect(joining("\n")) + "\n");
 
         bufferedReader.close();
         bufferedWriter.close();
